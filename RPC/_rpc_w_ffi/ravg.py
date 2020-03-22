03b50ca78a197c914d537c2efc4b8ca154cd1f34
srh <stephenrosshannam@gmail.com>
2020-03-23 06:31:42 +1100

    fixed: missing symbols in .so

--- RPC/_rpc_w_ffi/ravg.py
+++ RPC/_rpc_w_ffi/ravg.py
@@ -2,16 +2,16 @@ from cffi import FFI
 
 ffi = FFI()
 
-lib = ffi.dlopen('./ravg.so')
+lib = ffi.dlopen('./_ravg.so')
 print('Loaded lib {0}'.format(lib))
 
 # Describe the data type and function prototype to cffi.
-ffi.cdef("int main(int argc, char * argv[]);")
+ffi.cdef("int _main(int argc, char * argv[]);")
 
 print('Calling ravg_main via cffi')
 # Interesting variation: passing invalid arguments to add_data will trigger
 # a cffi type-checking exception.
-lib.main(4,[ffi.new("char[]", "localhost"),\
-            ffi.new("char[]", "123456"),\
-            ffi.new("char[]", "2458"),\
-            ffi.new("char[]", "58")])
+lib._main(5,[ffi.new("char[]", bytes("localhost", 'ascii')),\
+             ffi.new("char[]", bytes("123456", 'ascii')),\
+             ffi.new("char[]", bytes("2458", 'ascii')),\
+             ffi.new("char[]", bytes("58", 'ascii'))])
