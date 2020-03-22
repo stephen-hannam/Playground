ad060cd15425b015ed2017bd8faf877b8c3a9590
srh <stephenrosshannam@gmail.com>
2020-03-22 17:59:37 +1100

    still debugging passing of char *argv[] to a main-like

--- RPC/_rpc_w_ffi/FFI/hello.c
+++ RPC/_rpc_w_ffi/FFI/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char ** argv){
+int main(int argc, char argv){
   printf("hello world\n");
-  printf("hello %s", argv[1]);
+  //printf("hello %s", argv[1]);
+  if (argv == 'y')
+  {
+    printf("y");
+  }
   return 0;
 }
--- RPC/_rpc_w_ffi/FFI/hello.py
+++ RPC/_rpc_w_ffi/FFI/hello.py
@@ -6,8 +6,17 @@ lib = ffi.dlopen('./hello.so')
 print('Loaded lib {0}'.format(lib))
 
 # Describe the data type and function prototype to cffi.
-ffi.cdef("int main(int argc, char ** argv);")
+ffi.cdef("""
+int main(int argc, char argv);
+""")
 
 # Interesting variation: passing invalid arguments to add_data will trigger
 # a cffi type-checking exception.
-lib.main(2, ffi.new("char[]", "you".encode('ascii')))
+#a = ffi.new("char", bytes('you', 'ascii'))
+#b = ffi.new("char *[]", a)
+#print(a)
+#print(ffi.unpack(b[0], 4))
+#print(dir(a))
+#print(dir(b))
+ret = lib.main(2, bytes('y', 'ascii'))
+print(ret)
